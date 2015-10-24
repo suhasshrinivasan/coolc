a1c9e948c45256fee7e8d939b50004d40700441f
Suhas Shrinivasan <suhasshrinivasan@users.noreply.github.com>
2015-10-25 00:52:10 +0530

    Update lexer.h

--- lexer.h
+++ lexer.h
@@ -8,12 +8,12 @@
 #include <stdio.h>
 //#include "myhash.h"
 
-#define MAXTOKENLENGTH   40
+#define MAXTOKENLENGTH   400 // LOOK INTO THIS
 #define MAXKEYWORDLENGTH 8
 #define NUMBEROFKEYWORDS 19
 #define BUFSIZE			 256
-#define M 				 19  // Hashtable size. Number of keywords.
-#define ENHASH           false  // enable hashLookup for keywordLookup.
+// #define M 				 19     // Hashtable size. Number of keywords.
+// #define ENHASH           false  // enable hashLookup for keywordLookup.
 
 /* tokenString contains the current token value. */
 char tokenString[MAXTOKENLENGTH+1];
@@ -27,23 +27,26 @@ FILE* source;
 /* DFA state assignments */
 typedef enum
 { 
-	START, INASSIGN, INCOMMENT, ENDCOMMENT, INNUM, INID, INLESSTHAN, INHYPHEN, DONE
+	START, INASSIGN, INCOMMENT, ENDCOMMENT, INNUM, INID, INLESSTHAN, INHYPHEN, INPAREN, INSTRING, INSLASH, DONE
 } StateType;
 
 /* token type assigments */
 typedef enum
 { 
 	ID, NUM, STRING, COMMENT,
-	// Single symbols
-	PLUS, MINUS, MUL, DIV, OPEN_PAREN, CLOSE_PAREN, OPEN_CURLY_BRACE, CLOSE_CURLY_BRACE, OPEN_SQUARE_BRACE, CLOSE_SQUARE_BRACE, COLON, SEMICOLON, 
-	// Double symbols
-	ASSIGN, EQUIVALENCE, TILDE, LESS_THAN_EQUAL, LESS_THAN, NOT, DOT, AT_RATE_OF, 
+	PLUS, MINUS, MUL, DIV, 
+	OPEN_PAREN, CLOSE_PAREN, OPEN_CURLY_BRACE, CLOSE_CURLY_BRACE, 
+	OPEN_SQUARE_BRACE, CLOSE_SQUARE_BRACE, COLON, SEMICOLON, 
+	COMMA, ASSIGN, EQUIVALENCE, TILDE, 
+	GREATER_THAN, LESS_THAN_EQUAL, LESS_THAN, NOT, 
+	DOT, AT_RATE_OF,
 	// Key words
 	_CLASS, _ELSE, _FALSE, _FI, _IF, _IN, _INHERITS, _ISVOID, _LET, _LOOP, _POOL, _THEN, _WHILE, _CASE, _ESAC, _NEW, _OF, _NOT, _TRUE,
 	// Unidentified token
 	ERROR 
 } TokenType;
 
+/* Token String list */
 extern char TokenString[][50];
 
 /* Keywords table */
@@ -53,8 +56,17 @@ typedef struct KeywordStruct
 	TokenType token;
 } KeywordRecord;
 
+/* Keyword List */
 extern KeywordRecord KeywordList[NUMBEROFKEYWORDS];
 
+/* stderr. */
+extern unsigned int lineNumber;		// number of the current line.
+extern unsigned int linePos;		// position in the current line.
+
+/* For single lookahead in parsing. */
+extern TokenType prevToken;
+extern int prevLinePos;
+extern TokenType* currentTokenPointer;
 
 /* token record structure */
 typedef struct TokenAttributeStruct
@@ -70,12 +82,12 @@ typedef struct TokenAttributeStruct
 /* TokenType getToken() is the principle function of the lexer. 
  * It identifies and returns the TokenType of the current token.
  */ 
-TokenType getToken();
+extern TokenType getToken();
 
 /* getNextChar() gets the next character from the currently being scanned source file line. */
 char getNextChar();
 
 /* ungetChar() backtracks one  in the currently being scanned source file line. */
-void ungetChar();
+extern void ungetChar();
 
 #endif
